ca94bd68c570474029c445744c438ff73b0e6d69
Darren <darren.broe@iconplc.com>
2018-11-28 16:01:04 +0000

    added regex + amplifier for ut ogh'ing

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
 client.on('message', message => {
-	if (message.content === 'ping') {
-		message.channel.send('pong');
+	const uo = message.content.match(/u+\s*t+\s*o+\s*g+\s*h+/i);
+	if (uo) {
+		const s = uo[0].toUpperCase();
+		const us = uo[0].match(/u/ig).join('').length;
+		const os = uo[0].match(/o/ig).join('').length;
+		
+		let result = '';
+		[...new Set(s)].forEach(v => {
+			result += s.match(new RegExp(v, 'ig')).join('');
+			if (v === 'U') {
+				while (result.length < us * 2) result += v;
+			}
+			if (v === 'O') {
+				const lengthSoFar = result.length;
+				while (result.length - lengthSoFar < os * 2) result += v;
+			}
+		});
+		result = `***${result}***`;
+
+		message.channel.send(result);
 	}
 });
 
