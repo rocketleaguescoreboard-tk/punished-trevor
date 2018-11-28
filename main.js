46f1454c1c5fdf5930d7c30305bdc8274b03914d
ifreann <darren1337@gmail.com>
2018-11-28 19:56:55 +0000

    fixed mock()

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function mock(message) {
 	if (message.content.startsWith('mock')) {
 		const spongeCase = s => s.toLowerCase().split('').map((v, i) => i % 2 === 0 ? v : v.toUpperCase()).join('');
 		const users = message.mentions.users;
-		const user = users.entries();
+		const user = Array.from(users.entries())[0];
 		console.log('\n\n\n');
 		console.log(user);
 		console.log('\n\n\n');
