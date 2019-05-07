de27196e6ed3cb7551b03a86e669e4d4515395af
Chris Busse <chrismbusse@gmail.com>
2019-05-07 17:15:07 -0400

    upping mike pooping chances and adding logging

--- main.js
+++ main.js
@@ -383,14 +383,16 @@ function addOs(str) {
 
 // randomly decided whether to poop on Mike
 function poopMike(message) {
-	const addChance = 2;
+	const addChance = 4;
 	const randomNum = (Math.random() * 100);
 
 	// Determine whether to poop Mike or not
 	if (randomNum >= addChance) {
 		// If result is greater than likelihood, end.
+		console.info('Mike has narrowly escaped pooping');
 		return;
 	}
 	// poop on Mike
 	message.react('💩');
+	console.info("Mike's been pooped");
 }
